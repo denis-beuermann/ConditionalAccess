5d0f0137371226598a6ac5b5c9075cd05573b52a
Denis Beuermann <denis.beuermann@sefe.eu>
2025-11-10 11:18:33 +0100

    add new resource

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azuread_named_location" "Lubmin2" {
+  display_name = "location1"
+  ip {
+    ip_ranges = [
+      "45.142.20.72/32"
+    ]
+    trusted = false
+  }
 }
